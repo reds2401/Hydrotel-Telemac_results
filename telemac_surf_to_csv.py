3ac0ffc8cf0a5961d0f76ed280b59c7f427387f4
Sergio Redondo <71886119+reds2401@users.noreply.github.com>
2023-03-01 10:19:44 -0500

    Add files via upload

--- telemac_surf_to_csv.py
+++ telemac_surf_to_csv.py
@@ -20,43 +20,53 @@ main_folder = 'C:/Telemac_projects/'
 bathy_folders = [bf for bf in os.listdir(main_folder) if 'Bathy' in bf]        # Read only bathymetry folders
 bathyms = bathy_folders[0:4]
 
-#%% Read telemac files and write them to easier csv format
 result_folder = '/Results_20230215/'
 write_folder = 'C:/Users/reds2401/OneDrive - USherbrooke/Research_project/Maps/QGIS_Model/Flood_results_202302/'
+
+# %% Function
+def telemac2csv(r_var, w_var, r_fold, w_fold, par):
+    """
+    Function to read specific .xyz Telemac2D files and write them to csv.
+    Works for maximum and minimum depth and maximum velocity.
+    
+    Parameters
+    ----------
+    r_var : string
+        Name of files for a specific variable to read.
+    w_var : string
+        String for the name of the files to write.
+    r_fold : string
+        Directory to read the result files.
+    w_fold : string
+        Directory to write the files
+    par : string
+        Parameter to read from the results file.
+
+    Returns
+    -------
+    None.
+    """
+    filelist = [cas for cas in os.listdir(r_fold)
+                    if all(x in cas for x in [r_var])]                     # List Specific variable files
+    for file in filelist:
+        var_df = pd.read_csv(file, sep='\s+', header=None,
+                            skiprows=13, names = ['X', 'Y', par])           # Read results file
+        var_df[par][var_df[par] < 0.0001] = 0                         # Make all negative and small values to zero
+        file_name = w_var+file.replace(r_var,'')[:-4]+'.csv'
+        var_df.to_csv(w_fold+file_name,index = False)                    # Save dataframe to csv file
+
+#%% Read telemac files and write them to csv format easier to read in QGIS
+
 for bf in bathyms :
-    res_folder = main_folder+bf+result_folder                                   # Results folder path
-    os.chdir(res_folder)                                                        # Change directory to results folder
+    res_dir = main_folder+bf+result_folder                                   # Results folder path
+    os.chdir(res_dir)                                                        # Change directory to results folder
     
     #%% Write MaxDepth files
-    maxd_df = pd.DataFrame()                                                    # Initialize maxdepth hydrograph dataframe
-    max_filelist = [cas for cas in os.listdir(res_folder)
-                    if all(x in cas for x in ['MaxDepth'])]                     # List MaxDepth files
-    for file in max_filelist:
-        maxd_df = pd.read_csv(file, sep='\s+', header=None,
-                            skiprows=13, names = ['X', 'Y', 'Depth'])           # Read maxdepth results file
-        maxd_df['Depth'][maxd_df['Depth'] < 0.0001] = 0                         # Make all negative and small depths to zero
-        file_name = 'Dmax_'+file[9:-4]+'.csv'
-        maxd_df.to_csv(write_folder+file_name,index = False)                    # Save dataframe to csv file
-        
+    telemac2csv('MaxDepth', 'Dmax', res_dir, write_folder, 'Depth')
+    
     #%% Write MinDepth files
-    mind_df = pd.DataFrame()
-    min_filelist = [cas for cas in os.listdir(res_folder)
-                    if all(x in cas for x in ['MinDepth'])]                     # List MinDepth files
-    for file in min_filelist:
-        mind_df = pd.read_csv(file, sep='\s+', header=None,
-                            skiprows=13, names = ['X', 'Y', 'Depth'])           # Read mindepth results file
-        mind_df['Depth'][mind_df['Depth'] < 0.0001] = 0                         # Make all negative and small depths to zero
-        file_name = 'Dmin_'+file[9:-4]+'.csv'
-        mind_df.to_csv(write_folder+file_name,index = False)                    # Save dataframe to csv file   
-
+    telemac2csv('MinDepth', 'Dmin', res_dir, write_folder, 'Depth')
+    
     #%% Write MaxVel files
-    maxv_df = pd.DataFrame()
-    vel_filelist = [cas for cas in os.listdir(res_folder)
-                    if all(x in cas for x in ['MaxVel'])]                       # List MaxVel files
-    for file in vel_filelist:
-        maxv_df = pd.read_csv(file, sep='\s+', header=None,
-                            skiprows=13, names = ['X', 'Y', 'Velocity'])        # Read MaxVel results file
-        maxv_df['Velocity'][maxv_df['Velocity'] < 0.001] = 0                    # Make all small velocities to zero
-        file_name = 'Vmax_'+file[7:-4]+'.csv'
-        maxv_df.to_csv(write_folder+file_name,index = False)                    # Save dataframe to csv file
-        
+    telemac2csv('MaxVel', 'Vmax', res_dir, write_folder, 'Velocity')
+
