3abf3468fa5a7e285badff75ce7b9957ece6ce5d
Sergio Redondo <71886119+reds2401@users.noreply.github.com>
2023-03-03 12:21:36 -0500

    Add files via upload
    Syntax update

--- telemac_surf_to_csv.py
+++ telemac_surf_to_csv.py
@@ -16,8 +16,8 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #%% Files
-main_folder = 'C:/Telemac_projects/'
-bathy_folders = [bf for bf in os.listdir(main_folder) if 'Bathy' in bf]        # Read only bathymetry folders
+t_folder = 'C:/Telemac_projects/'                                              # Folder containing telemac projects 
+bathy_folders = [bf for bf in os.listdir(t_folder) if 'Bathy' in bf]           # Read only bathymetry folders
 bathyms = bathy_folders[0:4]
 
 result_folder = '/Results_20230215/'
@@ -47,18 +47,18 @@ def telemac2csv(r_var, w_var, r_fold, w_fold, par):
     None.
     """
     filelist = [cas for cas in os.listdir(r_fold)
-                    if all(x in cas for x in [r_var])]                     # List Specific variable files
+                    if all(x in cas for x in [r_var])]                       # List Specific variable files
     for file in filelist:
         var_df = pd.read_csv(file, sep='\s+', header=None,
-                            skiprows=13, names = ['X', 'Y', par])           # Read results file
-        var_df[par][var_df[par] < 0.0001] = 0                         # Make all negative and small values to zero
+                            skiprows=13, names = ['X', 'Y', par])            # Read results file
+        var_df[par][var_df[par] < 0.0001] = 0                                # Make all negative and small values to zero
         file_name = w_var+file.replace(r_var,'')[:-4]+'.csv'
-        var_df.to_csv(w_fold+file_name,index = False)                    # Save dataframe to csv file
+        var_df.to_csv(w_fold+file_name,index = False)                        # Save dataframe to csv file
 
 #%% Read telemac files and write them to csv format easier to read in QGIS
 
 for bf in bathyms :
-    res_dir = main_folder+bf+result_folder                                   # Results folder path
+    res_dir = t_folder+bf+result_folder                                      # Results folder path
     os.chdir(res_dir)                                                        # Change directory to results folder
     
     #%% Write MaxDepth files
